ce036694a9831f6e185864eebd73accbf42dc800
rgoldenb@yahoo.com.br <rgoldenb@yahoo.com.br>
2020-04-29 20:15:40 -0300

    Removed headers

--- ios/FlowsenseSDK.framework/PrivateHeaders/FlowsenseSDK.h
+++ ios/FlowsenseSDK.framework/PrivateHeaders/FlowsenseSDK.h
@@ -1,8 +1,8 @@
-#import <UIKit/UIKit.h>
-#import <FlowsenseSDK/Service_fs.h>
-#import <FlowsenseSDK/LocationTracker.h>
-#import <FlowsenseSDK/BackgroundTaskManager.h>
-#import <FlowsenseSDK/LocationShareModel.h>
+//#import <UIKit/UIKit.h>
+//#import <FlowsenseSDK/Service_fs.h>
+//#import <FlowsenseSDK/LocationTracker.h>
+//#import <FlowsenseSDK/BackgroundTaskManager.h>
+//#import <FlowsenseSDK/LocationShareModel.h>
 
 //! Project version number for SDK.
 FOUNDATION_EXPORT double SDKVersionNumber;
